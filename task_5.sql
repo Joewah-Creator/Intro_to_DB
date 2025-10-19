fbb66e710ceca3bfbb1735ffa9a38c1d3d9f8c64
josphat wahome <wahomejosphatt27@gmail.com>
2025-10-19 20:43:07 +0300

    Fix ; correct INSERT INTO customer querry for task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,7 @@
+-- Switch to the correct database
 USE alx_book_store;
-INSERT INTO Customers (customer_id, customer_name, email, address)
+
+-- Insert a single row into the customer table
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
-SELECT * FROM Customers;
+
