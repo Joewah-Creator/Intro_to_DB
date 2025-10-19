8dda361726a93c7af3aede592faa3a00b19f4524
josphat wahome <wahomejosphatt27@gmail.com>
2025-10-19 21:30:29 +0300

    Add tast_6.sql insert customers with exact adresses

--- task_6.sql
+++ task_6.sql
@@ -1,9 +1,11 @@
 -- Switch to the correct database
 USE alx_book_store;
 
--- Insert multiple rows into the customers table
-INSERT INTO customers (customer_id, customer_name, email, address)
-VALUES 
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+-- Script that inserts multiple rows into the table customer
+-- from the database passed as an argument
+
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
